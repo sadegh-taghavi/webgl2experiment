2f42383f0973ec9d1a4115c10cb5095c89b51f19
ST <taghavi.s.sadegh@gmail.com>
2022-09-30 12:19:23 +0330

    add liquidfun and express server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('./'))
+
+app.get('/', (req, res) => {
+  res.send('Test OK')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
